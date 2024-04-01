557c8c8e73e2bd3bfb2ee8180bdd73f8c1f54bfb
bhtu <bhtu.work@gmail.com>
2024-04-01 22:23:52 +0700

    half complete lexical parser

--- main.c
+++ main.c
@@ -58,14 +58,82 @@ typedef struct lexical_stream
 } LexicalStream;
 
 
-Token nextToken(LexicalStream lexicalStream) {
-    char ch = lexicalStream.lastChar;
-
-    do {
-
-    } while(ch = fgetc(lexicalStream.source));
-
+Token nextToken(LexicalStream *lexicalStream) {
+    Token token;
+    char c = lexicalStream->lastChar;
     
+    // Skip whitespace
+    while (isspace(c)) {
+        c = fgetc(lexicalStream->source);
+    } 
+    
+    // End of file
+    if (c == EOF) {
+        token.type = NONE;
+        return token;
+    }
+    
+    // Check for single-character tokens
+    switch (c) {
+        case '+': token.type = PLUS; break;
+        case '-': token.type = MINUS; break;
+        case '*': token.type = TIMES; break;
+        case '/': token.type = SLASH; break;
+        case '=': token.type = EQU; break;
+        case '<':
+            if ((c = fgetc(lexicalStream->source)) == '>') {
+                token.type = NEQ;
+            } else if (c == '=') {
+                token.type = LEQ;
+            } else {
+                lexicalStream->lastChar = c;
+                token.type = LSS;
+            }
+            break;
+        case '>':
+            if ((c = fgetc(lexicalStream->source)) == '=') {
+                token.type = GEQ;
+            } else {
+                lexicalStream->lastChar = c;
+                token.type = GTR;
+            }
+            break;
+        case '(': token.type = LPARENT; break;
+        case ')': token.type = RPARENT; break;
+        case '[': token.type = LBRACK; break;
+        case ']': token.type = RBRACK; break;
+        case '.': token.type = PERIOD; break;
+        case ',': token.type = COMMA; break;
+        case ';': token.type = SEMICOLON; break;
+        case '%': token.type = PERCENT; break;
+        default:
+            // Check for identifiers or numbers
+            if (isdigit(c)) {
+                token.type = NUMBER;
+                token.number = c - '0';
+                while (isdigit(c = fgetc(lexicalStream->source))) {
+                    token.number = token.number * 10 + (c - '0');
+                }
+                lexicalStream->lastChar = c;
+            } else if (isalpha(c)) {
+                token.type = IDENT;
+                token.id[0] = c;
+                int i = 1;
+                while (isalnum(c = fgetc(lexicalStream->source)) && i <= IDENT_MAX_LEN) {
+                    token.id[i++] = c;
+                }
+                token.id[i] = '\0';
+                lexicalStream->lastChar = c;
+
+                // TODO: check other keywords
+            } else {
+                // TODO: Handle unrecognized characters
+                token.type = NONE;
+            }
+            break;
+    }
+    
+    return token;
 }
 
 int main(int argc, char const *argv[])
