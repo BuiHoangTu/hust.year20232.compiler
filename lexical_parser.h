085dd16a0e60f69a2ba70cd4e6ebb90033fe53da
bhtu <bhtu.work@gmail.com>
2024-04-02 13:10:22 +0700

    moving lexical parser to other file

---
+++ lexical_parser.h
@@ -0,0 +1,9 @@
+#ifndef LEXICAL_PARSER
+#define LEXICAL_PARSER
+
+#include <stdio.h>
+
+// Include the necessary definitions from lexical_parser.c
+#include "lexical_parser.c"
+
+#endif // LEXICAL_PARSER
