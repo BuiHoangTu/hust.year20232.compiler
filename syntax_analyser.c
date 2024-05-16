5c0266fbb8a0d3f9bbec12094421e559b52a0f2f
bhtu <bhtu.work@gmail.com>
2024-05-16 21:21:11 +0700

    add structure fore syntax

---
+++ syntax_analyser.c
@@ -0,0 +1,37 @@
+//
+// Created by tu on 16/05/2024.
+//
+
+#include "syntax_analyser.h"
+#include "lexical_parser.h"
+
+Token token;
+
+
+void factor() {
+
+}
+
+void term() {
+
+}
+
+void expression() {
+
+}
+
+void condition() {
+
+}
+
+void statement() {
+
+}
+
+void block() {
+
+}
+
+void program() {
+
+}
---
+++ syntax_analyser.h
@@ -0,0 +1,16 @@
+//
+// Created by tu on 16/05/2024.
+//
+
+#ifndef INYEAR4_2_COMPILER_SYNTAX_ANALYSER_H
+#define INYEAR4_2_COMPILER_SYNTAX_ANALYSER_H
+
+void factor();
+void term();
+void expression();
+void condition();
+void statement();
+void block();
+void program();
+
+#endif //INYEAR4_2_COMPILER_SYNTAX_ANALYSER_H
